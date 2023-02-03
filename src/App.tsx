7875c7f6067382bcdea9744c6ae4f9d7bb38e2c1
JosielMatos <josielmatos77@protonmail.com>
2023-02-03 10:15:55 -0300

    Updated dependencies, removed unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { AdminRoom } from "./pages/AdminRoom";
 import { Room } from "./pages/Room";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
@@ -14,7 +13,6 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/rooms/new' element={<NewRoom />} />
           <Route path='/rooms/:id' element={<Room />} />
-          <Route path='/admin/rooms/:id' element={<AdminRoom />} />
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -3,64 +3,74 @@ import { database } from "../services/firebase";
 import { useAuth } from "./useAuth";
 
 type CommentType = {
-    id: string,
-    author: {
-        name: string;
-        avatar: string;
-    }
-    content: string;
-    isAnswered: boolean;
-    isHighlighted: boolean;
-    likeCount: number;
-    likeId: string | undefined;
-}
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
+};
 
-type FirebaseComments = Record<string, {
+type FirebaseComments = Record<
+  string,
+  {
     author: {
-        name: string;
-        avatar: string;
-    }
+      name: string;
+      avatar: string;
+    };
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
-    likes: Record<string, {
+    likes: Record<
+      string,
+      {
         authorId: string;
-    }>
-}>
+      }
+    >;
+  }
+>;
 
 export function useRoom(RoomId: string) {
-    const { user } = useAuth();
-    const [title, setTitle] = useState('');
-    const [videoUrl, setVideoUrl] = useState('');
-    const [comments, setComments] = useState<CommentType[]>([])
+  const { user } = useAuth();
+  const [title, setTitle] = useState("");
+  const [videoUrl, setVideoUrl] = useState("");
+  const [comments, setComments] = useState<CommentType[]>([]);
 
-    useEffect(() => {
-        const roomsRef = database.ref(`rooms/${RoomId}`);
+  useEffect(() => {
+    const roomsRef = database.ref(`rooms/${RoomId}`);
 
-        roomsRef.on('value', room => {
-            const databaseRoom = room.val();
-            const firebaseComments: FirebaseComments = databaseRoom.comments ?? {};
-            const parsedComments = Object.entries(firebaseComments).map(([key, value]) => {
-                return {
-                    id: key,
-                    content: value.content,
-                    author: value.author,
-                    isHighlighted: value.isHighlighted,
-                    isAnswered: value.isAnswered,
-                    likeCount: Object.values(value.likes ?? {}).length,
-                    likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
-                }
-            })
-
-            setTitle(databaseRoom.title)
-            setVideoUrl(databaseRoom.videoUrl)
-            setComments(parsedComments)
-        })
-
-        return () => {
-            roomsRef.off('value');
+    roomsRef.on("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseComments: FirebaseComments = databaseRoom.comments ?? {};
+      const parsedComments = Object.entries(firebaseComments).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(
+              ([key, like]) => like.authorId === user?.id
+            )?.[0],
+          };
         }
-    }, [RoomId, user?.id])
+      );
 
-    return { comments, title, videoUrl }
+      setTitle(databaseRoom.title);
+      setVideoUrl(databaseRoom.videoUrl);
+      setComments(parsedComments);
+    });
+
+    return () => {
+      roomsRef.off("value");
+    };
+  }, [RoomId, user?.id]);
+
+  return { comments, title, videoUrl };
 }
